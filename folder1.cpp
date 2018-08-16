c33dda8d09501d249b485c648c3b26b1d6369d75
shiktapriyadarsini <42140757+shiktapriyadarsini@users.noreply.github.com>
2018-08-16 14:27:42 +0000

    Add files via upload

---
+++ folder1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+// write the main function
+int main(){
+// declaring the variables
+int a,b;
+// assinging the values
+a=4 ;
+b=2 ;
+cout << "sum" << a+b <<endl;
+cout << "sub" << a-b <<endl;
+cout << "multiply" << a*b <<endl;
+cout << "division" << a/b <<endl;
+cout << "modulus" << a%b <<endl;
+}
+           
