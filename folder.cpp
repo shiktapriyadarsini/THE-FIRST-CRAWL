53626fa5e49ed0d11ba6281b62e5c7f92c356835
shiktapriyadarsini <42140757+shiktapriyadarsini@users.noreply.github.com>
2018-08-16 14:20:44 +0000

    Add files via upload

---
+++ folder.cpp
@@ -0,0 +1,17 @@
+// first include the library
+#include <iostream>
+using namespace std;
+// write the main function
+int main(){
+// declaring the variables
+int a,b;
+// assinging the values
+a=4 ;
+b=2 ;
+cout<< "sum" << a+b <<endl;
+cout<< "sub" << a-b <<endl;
+cout<< "multiply" << a*b <<endl;
+count << "division" << a/b <<endl;
+cout << "modulus" << a%b <<endl;
+}
+           
